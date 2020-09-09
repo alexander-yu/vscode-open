0a5ad11df092910f07a126dc87355a2288b60b2e
Alex Yu <yu.alex96@gmail.com>
2020-09-09 03:43:01 -0400

    Clean up variables.ts

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,15 @@
 const VARIABLE_REGEX = /\$\{(.*?)\}/g;
-const FILE_REGEX_STR = '.*';
+
+const VARIABLE_HANDLERS = new Map<string, (context: Context, value: string, argument: string | undefined) => string>([
+    ['env', evaluateEnv],
+    ['match', evaluateMatch],
+    ['editor', evaluateEditor],
+    ['regex', evaluateRegex],
+]);
+
+const REGEX_TEMPLATES = new Map<string, string>([
+    ['file', '.*'],
+]);
 
 // TODO (ayu): docstrings
 
@@ -7,6 +17,59 @@ export interface Context {
     env: NodeJS.ProcessEnv,
     lines: [number, number] | null,
     match: RegExpExecArray | null,
+}
+
+function evaluateEnv(context: Context, value: string, variable: string | undefined): string {
+    if (!variable) {
+        throw new Error(`${value} cannot be resolved because no environment variable name is given.`);
+    }
+    const env = context.env[variable];
+    return (typeof env === 'string') ? env : '';
+}
+
+function evaluateMatch(context: Context, value: string, group: string | undefined): string {
+    // TODO (ayu): validation against the argument; is the argument validly formed?
+    // Does the argument exist in the match?
+    // Handle group numbers too
+    if (!context.match) {
+        throw new Error(`${value} cannot be resolved because there is no regex match`);
+    } else if (!context.match.groups) {
+        throw new Error(`${value} cannot be resolved because there are no regex capture groups to match`);
+    } else if (!group) {
+        throw new Error(`${value} cannot be resolved because no regex capture group is given`);
+    }
+    return context.match.groups[group];
+}
+
+function evaluateEditor(context: Context, value: string, variable: string | undefined): string {
+    if (!variable) {
+        throw new Error(`${value} cannot be resolved because no editor variable name is given.`);
+    }
+
+    switch (variable) {
+        case 'lines':
+            if (!context.lines) {
+                return '';
+            } else if (context.lines[0] === context.lines[1]) {
+                return `#${context.lines[0].toString()}`;
+            }
+            return `#${context.lines[0].toString()}-${context.lines[1].toString()}`;
+        default:
+            throw new Error(`${variable} is not a valid editor variable.`);
+    }
+}
+
+function evaluateRegex(_: Context, value: string, template: string | undefined): string {
+    if (!template) {
+        throw new Error(`${value} cannot be resolved because no regex template name is given.`);
+    }
+
+    const regex = REGEX_TEMPLATES.get(template);
+    if (!regex) {
+        throw new Error(`${template} is not a valid regex template.`);
+    }
+
+    return `(?<${template}>${regex})`;
 }
 
 function evaluate(value: string, variable: string, context: Context): string {
@@ -18,60 +81,12 @@ function evaluate(value: string, variable: string, context: Context): string {
         argument = parts[1];
     }
 
-    switch (variable) {
-        case 'env':
-            if (!argument) {
-                throw new Error(`${value} cannot be resolved because no environment variable name is given.`);
-            }
-            const env = context.env[argument];
-            return (typeof env === 'string') ? env : '';
-        case 'match':
-            // TODO (ayu): validation against the argument; is the argument validly formed?
-            // Does the argument exist in the match?
-            // Handle group numbers too
-            if (!context.match) {
-                throw new Error(`${value} cannot be resolved because there is no regex match`);
-            } else if (!context.match.groups) {
-                throw new Error(`${value} cannot be resolved because there are no regex capture groups to match`);
-            } else if (!argument) {
-                throw new Error(`${value} cannot be resolved because no regex capture group is given`);
-            }
-            return context.match.groups[argument];
-        case 'editor':
-            if (!argument) {
-                throw new Error(`${value} cannot be resolved because no editor variable name is given.`);
-            }
-
-            switch (argument) {
-                case 'lines':
-                    if (!context.lines) {
-                        return '';
-                    } else if (context.lines[0] === context.lines[1]) {
-                        return `#${context.lines[0].toString()}`;
-                    }
-                    return `#${context.lines[0].toString()}-${context.lines[1].toString()}`;
-                default:
-                    throw new Error(`${argument} is not a valid editor variable.`);
-            }
-        case 'regex':
-            if (!argument) {
-                throw new Error(`${value} cannot be resolved because no regex template name is given.`);
-            }
-
-            let regex = null;
-
-            switch (argument) {
-                case 'file':
-                    regex = FILE_REGEX_STR;
-                    break;
-                default:
-                    throw new Error(`${argument} is not a valid regex template.`);
-            }
-
-            return `(?<${argument}>${regex})`;
-        default:
-            return value;
+    const handler = VARIABLE_HANDLERS.get(variable);
+    if (handler) {
+        return handler(context, value, argument);
     }
+
+    return value;
 }
 
 export function resolve(value: string, context: Context): string {
